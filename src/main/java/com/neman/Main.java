bd212d6dcc89198c7843f456d5985b11f4b0e42e
Nemanja Vukosavljevic <nemanja06@gmail.com>
2014-08-24 22:16:34 +0200

    Changed port to 8081

--- src/main/java/com/neman/Main.java
+++ src/main/java/com/neman/Main.java
@@ -17,7 +17,7 @@ import java.util.concurrent.Executors;
  */
 public class Main {
 
-    private static final int PORT = 9000;
+    private static final int PORT = 8081;
 
 
     public static void main(String[] args) throws IOException {
